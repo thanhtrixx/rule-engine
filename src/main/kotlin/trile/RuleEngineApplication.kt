df517cf0b9b2895717641ec2e7b708ee62cfaf45
Tri Le <thanhtrixx@gmail.com>
2024-12-28 19:41:40 +0700

    Implement RuleEngine flow Test RuleEngine

--- src/main/kotlin/trile/RuleEngineApplication.kt
+++ src/main/kotlin/trile/RuleEngineApplication.kt
@@ -1,18 +1,31 @@
 package trile
 
+import java.math.BigDecimal
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import trile.common.Log
+import trile.rule.engine.RuleEngine
 import trile.rule.model.RuleConfiguration
+import trile.rule.model.TransactionContext
 
 @SpringBootApplication
 class RuleEngineApplication(
   private val ruleConfig: RuleConfiguration,
+  private val ruleEngine: RuleEngine
 ) : ApplicationRunner, Log {
   override fun run(args: ApplicationArguments?) {
     l.info("Loaded rule config: [$ruleConfig]")
+
+
+    val context = TransactionContext(type = "regular-card", amount = BigDecimal.valueOf(10000))
+    l.info("Executing with context: [$context]")
+    ruleEngine.executeRules(context)
+
+    val notRunContext = TransactionContext(type = "regular-card", amount = BigDecimal.valueOf(10))
+    l.info("Executing with context: [$notRunContext]")
+    ruleEngine.executeRules(notRunContext)
   }
 }
 
--- src/main/kotlin/trile/rule/engine/RuleEngine.kt
+++ src/main/kotlin/trile/rule/engine/RuleEngine.kt
@@ -1,65 +1,73 @@
 package trile.rule.engine
 
 import org.springframework.stereotype.Service
+import trile.common.Log
+import trile.common.getOrThrow
 import trile.rule.action.Action
 import trile.rule.condition.Condition
 import trile.rule.model.RuleConfiguration
-import trile.rule.model.RuleContext
+import trile.rule.model.TransactionContext
 
 @Service
 class RuleEngine(
   ruleConfig: RuleConfiguration,
-  conditions: List<Condition<*>>,
-  actions: List<Action<*>>
-) {
+  conditions: List<Condition<Any>>,
+  actions: List<Action<Any>>
+) : Log {
 
-  private val conditionMap = conditions.associateBy { it.type }
+  private val conditionMap =
+    conditions.associateBy { it.type }.also { l.info("Loaded conditions [${it.keys.joinToString()}]") }
 
-  private val actionMap = actions.associateBy { it.type }
+  private val actionMap = actions.associateBy { it.type }.also { l.info("Loaded actions [${it.keys.joinToString()}]") }
 
-//  private val workersMapBySource: Map<String, RuleExecutor> =
-//    ruleConfig.purchaseTypes
-//      .map {
-//        it.key to RuleExecutor(it.value.name, createRules(it.value.rules))
-//      }.toMap()
-//
-//  private fun getRuleExecutors(source: String): RuleExecutor =
-//    workersMapBySource[source] ?: throw RuntimeException("Can not find rule executor for source: [$source]")
+  private val executorByType: Map<String, RuleExecutor> =
+    ruleConfig.paymentUseCases
+      .map {
+        it.key to RuleExecutor(it.value.name, createRules(it.value.rules))
+      }.toMap()
 
-//  private fun <T> RuleConfiguration.RuleSetDefinition.RuleDefinition.ConditionDefinition.toConditionWithParameter(): ConditionWithParameter<out T> {
-//    val condition = conditionMap[this.type] ?: throw RuntimeException("AA")
-//    return ConditionWithParameter(condition, condition.convertParameter(parameters))
-//  }
-//
-//
-//  private fun RuleConfiguration.RuleSetDefinition.RuleDefinition.ActionDefinition.toActionWrapper(): RuleSetWrapper.ActionWrapper {
-//    val action = actionMap[type]
-//      ?: throw RuntimeException("Can not find action for type: [${type}]")
-//    return RuleSetWrapper.ActionWrapper(action, action.convertParameter(parameters))
-//  }
-//
-//
-//  private fun createRules(ruleSetConfigurationDefinitions: List<RuleConfig.Rule.RuleSet.RuleDefinition>): List<RuleSetWrapper> {
-//    return ruleSetConfigurationDefinitions.map { rule ->
-//      RuleSetWrapper(
-//        conditions = rule.conditions.map { it.toConditionWrapper() },
-//        actions = rule.actions.map { it.toActionWrapper() }
-//      )
-//    }
-//  }
-//
-//  private class RuleExecutor(
-//    private val name: String,
-//    private val rules: List<RuleSetWrapper>
-//  ) {
-//
-//    fun execute(context: RuleContext) {
-//      for (rule in rules) {
-//        if (rule.conditions.all { it.condition.evaluate(context, it.parameter) }) {
-//          rule.actions.forEach { it.action.execute(context, it.parameter) }
-//        }
-//      }
-//    }
-//  }
+  fun executeRules(context: TransactionContext) {
+    val executor = executorByType[context.type]
+    if (executor == null) {
+      l.info("Cannot get executor for type ${context.type}. Ignored this transaction")
+      return
+    }
+    executor.execute(context)
+  }
+
+  private fun RuleConfiguration.RuleSetDefinition.RuleDefinition.ConditionDefinition.toConditionWithParameter(): ConditionWithParameter<Any> {
+    val condition = conditionMap.getOrThrow(this.type, "Can not find condition for type [$type]")
+    return ConditionWithParameter(condition = condition, parameter = condition.convertParameter(parameters))
+  }
+
+
+  private fun RuleConfiguration.RuleSetDefinition.RuleDefinition.ActionDefinition.toActionWithParameter(): ActionWithParameter<Any> {
+    val action = actionMap.getOrThrow(type, "Can not find action for type: [${type}]")
+    return ActionWithParameter(action = action, parameter = action.convertParameter(parameters))
+  }
+
+
+  private fun createRules(ruleSetConfigurationDefinitions: List<RuleConfiguration.RuleSetDefinition.RuleDefinition>): List<RuleSetWrapper<Any, Any>> {
+    return ruleSetConfigurationDefinitions.map { rule ->
+      RuleSetWrapper(
+        conditions = rule.conditions.map { it.toConditionWithParameter() },
+        actions = rule.actions.map { it.toActionWithParameter() }
+      )
+    }
+  }
+
+  private class RuleExecutor(
+    private val name: String,
+    private val rules: List<RuleSetWrapper<Any, Any>>
+  ) {
+
+    fun execute(context: TransactionContext) {
+      for (rule in rules) {
+        if (rule.conditions.all { it.condition.evaluate(context, it.parameter) }) {
+          rule.actions.forEach { it.action.execute(context, it.parameter) }
+        }
+      }
+    }
+  }
 
 }
