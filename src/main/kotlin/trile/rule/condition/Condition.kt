56c2b83286e5654469a902e918d471d9bca27849
Tri Le <thanhtrixx@gmail.com>
2024-12-29 15:14:51 +0700

    Add ConditionType.OPERATOR

--- src/main/kotlin/trile/rule/condition/Condition.kt
+++ src/main/kotlin/trile/rule/condition/Condition.kt
@@ -1,5 +1,6 @@
 package trile.rule.condition
 
+import trile.rule.model.ConditionDefinition
 import trile.rule.model.TransactionContext
 
 
@@ -7,14 +8,15 @@ interface Condition<out T> {
 
   fun evaluate(context: TransactionContext, parameter: @UnsafeVariance T): Boolean
   val type: ConditionType
-  fun convertParameter(parameters: Map<String, String>): T
+  fun convertParameter(conditionDefinition: ConditionDefinition): T
 }
 
 enum class ConditionType {
+  OPERATOR,
   EQUALS,
   GREATER_THAN,
   LESS_THAN,
   IN_RANGE,
   IN_CATEGORIES,
-  CONTAINS
+  CONTAINS,
 }
--- src/main/kotlin/trile/rule/condition/EqualsCondition.kt
+++ src/main/kotlin/trile/rule/condition/EqualsCondition.kt
@@ -3,6 +3,7 @@ package trile.rule.condition
 import java.math.BigDecimal
 import org.springframework.stereotype.Component
 import trile.common.getAndToBigDecimal
+import trile.rule.model.ConditionDefinition
 import trile.rule.model.TransactionContext
 
 @Component
@@ -13,5 +14,6 @@ class EqualsCondition : Condition<BigDecimal> {
 
   override val type = ConditionType.EQUALS
 
-  override fun convertParameter(parameters: Map<String, String>) = parameters.getAndToBigDecimal("amount")
+  override fun convertParameter(conditionDefinition: ConditionDefinition) =
+    conditionDefinition.parameters.getAndToBigDecimal("amount")
 }
